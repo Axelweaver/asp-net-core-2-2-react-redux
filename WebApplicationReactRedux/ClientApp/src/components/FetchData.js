1cbf314eb6546c809cac174b93176eaf050ff1cd
Tkachev Aleksei <tkachev_ap@shopandshow.ru>
2019-05-08 07:51:47 +0300

    update action types

--- WebApplicationReactRedux/ClientApp/src/components/FetchData.js
+++ WebApplicationReactRedux/ClientApp/src/components/FetchData.js
@@ -5,70 +5,91 @@ import { Link } from 'react-router-dom';
 import { actionCreators } from '../reducers/WeatherForecasts';
 
 class FetchData extends Component {
-  componentDidMount() {
-    // This method is called when the component is first added to the document
-    this.ensureDataFetched();
-  }
+    componentDidMount() {
 
-  componentDidUpdate() {
-    // This method is called when the route parameters change
-    this.ensureDataFetched();
-  }
+        // This method is called when the component is first added to the document
+        this.setSearchParams();
+        this.ensureDataFetched();
+    }
 
-  ensureDataFetched() {
-    const startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
-    this.props.requestWeatherForecasts(startDateIndex);
-  }
+    componentDidUpdate() {
+        // This method is called when the route parameters change
+        this.setSearchParams();
+        this.ensureDataFetched();
+        
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
-        {renderForecastsTable(this.props)}
-        {renderPagination(this.props)}
-      </div>
-    );
-  }
+    setSearchParams() {
+        var searchParams = new URLSearchParams(this.props.location.search);
+        var page = searchParams.get("page");
+        var category = searchParams.get("category");
+        this.props.setSearchParamsWeatherForecasts(page, category);
+    }
+
+    ensureDataFetched() {
+        const startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
+        this.props.requestWeatherForecasts(startDateIndex);
+    }
+
+    render() {
+        var searchParams = new URLSearchParams(this.props.location.search);
+
+        return (
+            <div>
+                <p>Match: {JSON.stringify(this.props.match)}</p>
+                <p>Location {JSON.stringify(this.props.location)}</p>
+                <p>startDateIndex: {this.props.match.params.startDateIndex}</p>
+                <p>Page: {this.props.page}</p>
+                <p>Category: {this.props.category}</p>
+                <p>Sorted: {searchParams.get("sorted")}</p>
+                <h1>Weather forecast</h1>
+                <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
+                {renderForecastsTable(this.props)}
+                {renderPagination(this.props)}
+            </div>
+        );
+    }
 }
 
 function renderForecastsTable(props) {
-  return (
-    <table className='table table-striped'>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Temp. (C)</th>
-          <th>Temp. (F)</th>
-          <th>Summary</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.forecasts.map(forecast =>
-          <tr key={forecast.dateFormatted}>
-            <td>{forecast.dateFormatted}</td>
-            <td>{forecast.temperatureC}</td>
-            <td>{forecast.temperatureF}</td>
-            <td>{forecast.summary}</td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  );
+    return (
+        <table className='table table-striped'>
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Temp. (C)</th>
+                    <th>Temp. (F)</th>
+                    <th>Summary</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.forecasts.map(forecast =>
+                    <tr key={forecast.dateFormatted}>
+                        <td>{forecast.dateFormatted}</td>
+                        <td>{forecast.temperatureC}</td>
+                        <td>{forecast.temperatureF}</td>
+                        <td>{forecast.summary}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+    );
 }
 
 function renderPagination(props) {
-  const prevStartDateIndex = (props.startDateIndex || 0) - 5;
-  const nextStartDateIndex = (props.startDateIndex || 0) + 5;
+    const prevStartDateIndex = (props.startDateIndex || 0) - 5;
+    const nextStartDateIndex = (props.startDateIndex || 0) + 5;
 
-  return <p className='clearfix text-center'>
-    <Link className='btn btn-default pull-left' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
-    <Link className='btn btn-default pull-right' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
-    {props.isLoading ? <span>Loading...</span> : []}
-  </p>;
+    return <p className='clearfix text-center'>
+        <Link className='btn btn-default pull-left' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
+        <Link className='btn btn-default pull-right' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
+        <Link className='btn btn-warning pull-right' to="/fetch-data/?category=1&page=1">Test1</Link>
+        <Link className='btn btn-warning pull-right' to="/fetch-data/?category=2&page=2">Test2</Link>
+        {props.isLoading ? <span>Loading...</span> : []}
+    </p>;
 }
 
 export default connect(
-  state => state.weatherForecasts,
-  dispatch => bindActionCreators(actionCreators, dispatch)
+    state => state.weatherForecasts,
+    dispatch => bindActionCreators(actionCreators, dispatch)
 )(FetchData);
--- WebApplicationReactRedux/ClientApp/src/reducers/WeatherForecasts.js
+++ WebApplicationReactRedux/ClientApp/src/reducers/WeatherForecasts.js
@@ -3,41 +3,53 @@ import actionTypes from '../actions/actionTypes';
 const initialState = { forecasts: [], isLoading: false };
 
 export const actionCreators = {
-  requestWeatherForecasts: startDateIndex => async (dispatch, getState) => {
-    if (startDateIndex === getState().weatherForecasts.startDateIndex) {
-      // Don't issue a duplicate request (we already have or are loading the requested data)
-      return;
+    requestWeatherForecasts: startDateIndex => async (dispatch, getState) => {
+        if (startDateIndex === getState().weatherForecasts.startDateIndex) {
+            // Don't issue a duplicate request (we already have or are loading the requested data)
+            return;
+        }
+        console.log("FetchData actionCreators match params", getState().match);
+        console.log("FetchData actionCreators location.search", getState().location);
+        dispatch({ type: actionTypes.REQUEST_WEATHER_FORECASTS, startDateIndex });
+
+        const url = `api/SampleData/WeatherForecasts?startDateIndex=${startDateIndex}`;
+        const response = await fetch(url);
+
+        const forecasts = await response.json();
+
+        dispatch({ type: actionTypes.RECEIVE_WEATHER_FORECASTS, startDateIndex, forecasts });
+    },
+    setSearchParamsWeatherForecasts: (page, category) => (dispatch) => {
+        dispatch({ type: actionTypes.SET_PARAMS_WEATHER_FORECASTS, page, category });
     }
-
-    dispatch({ type: actionTypes.REQUEST_WEATHER_FORECASTS, startDateIndex });
-
-    const url = `api/SampleData/WeatherForecasts?startDateIndex=${startDateIndex}`;
-    const response = await fetch(url);
-    const forecasts = await response.json();
-
-    dispatch({ type: actionTypes.RECEIVE_WEATHER_FORECASTS, startDateIndex, forecasts });
-  }
 };
 
 export const reducer = (state, action) => {
-  state = state || initialState;
+    state = state || initialState;
 
-  if (action.type === actionTypes.REQUEST_WEATHER_FORECASTS) {
-    return {
-      ...state,
-      startDateIndex: action.startDateIndex,
-      isLoading: true
-    };
-  }
+    if (action.type === actionTypes.REQUEST_WEATHER_FORECASTS) {
+        return {
+            ...state,
+            startDateIndex: action.startDateIndex,
+            isLoading: true
+        };
+    }
 
-  if (action.type === actionTypes.RECEIVE_WEATHER_FORECASTS) {
-    return {
-      ...state,
-      startDateIndex: action.startDateIndex,
-      forecasts: action.forecasts,
-      isLoading: false
-    };
-  }
+    if (action.type === actionTypes.RECEIVE_WEATHER_FORECASTS) {
+        return {
+            ...state,
+            startDateIndex: action.startDateIndex,
+            forecasts: action.forecasts,
+            isLoading: false
+        };
+    }
 
-  return state;
+    if (action.type === actionTypes.SET_PARAMS_WEATHER_FORECASTS) {
+        return {
+            ...state,
+            page: action.page,
+            category: action.category
+        };
+    }
+    return state;
 };
